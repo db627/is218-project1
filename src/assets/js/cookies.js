8e014c0c8703b1b8ae63cd35126dce7977b63656
db627 <db627@njit.edu>
2023-03-17 16:42:21 -0400

    added cookie popup and translate

--- src/assets/js/cookies.js
+++ src/assets/js/cookies.js
@@ -1,17 +1,37 @@
-if (localStorage.getItem("cookiesAccepted")) {
-    // User has already accepted cookies
-  } else {
-    // Show the cookie popup
-    document.querySelector('.cookie-popup').classList.add('show');
-  }
-  
-  // Add an event listener to the accept cookies button
-  document.querySelector('#accept-cookies-btn').addEventListener('click', function() {
-    // Set a cookie to remember that the user has accepted cookies
-    localStorage.setItem("cookiesAccepted", true);
-    
-    // Hide the cookie popup
-    document.querySelector('.cookie-popup').classList.remove('show');
+document.addEventListener("DOMContentLoaded", function(){
+  var acceptCookiesBtn = document.getElementById('accept-cookies-btn');
+  acceptCookiesBtn.addEventListener("click", function(){
+    setCookie('cookiesAccepted', true, 30);
+    hideCookiePopup();
   });
 
-  
+  if (getCookie('cookiesAccepted') === 'true') {
+    hideCookiePopup();
+  }
+});
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function hideCookiePopup() {
+  var cookiePopupContainer = document.querySelector('.cookie-popup-container');
+  cookiePopupContainer.style.display = 'none';
+}
