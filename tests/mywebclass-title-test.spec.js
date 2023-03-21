63e43f35338a454aca3dd2fb936cc09945f896e0
Dennis Boguslavskiy <db627@njit.edu>
2023-03-21 12:43:35 -0400

    added playwright testing

--- tests/mywebclass-title-test.spec.js
+++ tests/mywebclass-title-test.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Page title is correct', async ({ page }) => {
-  await page.goto('https://localhost:5500');
+  await page.goto('https://db627.github.io/is218-project1/index.html');
   const pageTitle = await page.title();
   expect(pageTitle).toBe('MyWebClass: Revolutianized Teaching Solutions');
 });
