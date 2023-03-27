77bf65d4a04279fa6ea1504acc76bbf99284d74e
db627 <db627@njit.edu>
2023-03-26 20:39:25 -0400

    organized files

--- tests/mywebclass-navigation.spec.js
+++ tests/mywebclass-navigation.spec.js
@@ -1,9 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('test navigation works on web', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('https://db627.github.io/is218-project1/');
-  await page.getByRole('link', { name: 'MyWebClass' }).click();
+  await page.getByRole('link', { name: 'MyWebClass', exact: true }).click();
   await page.getByRole('link', { name: 'Home' }).click();
   await page.getByRole('link', { name: 'Article', exact: true }).click();
-  await page.getByRole('link', { name: 'Home' }).click();
 });
